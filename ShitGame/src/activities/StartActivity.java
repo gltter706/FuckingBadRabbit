d6e7d725b1004a6649d394b867a737c0c37b7b7e
gltter706 <gltter706@163.com>
2014-11-15 01:28:06 +0800

    新建工程以及代码规范
    新建工程以及代码规范

---
+++ ShitGame/src/activities/StartActivity.java
@@ -0,0 +1,13 @@
+package activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+}
