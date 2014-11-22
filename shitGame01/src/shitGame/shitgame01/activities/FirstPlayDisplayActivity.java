f68e56fcf1f90de149e790634c11cd8cb5640215
jasonCarNormal0101 <jasoncar@outlook.com>
2014-11-22 12:20:37 +0800

    Activities连接

---
+++ shitGame01/src/shitGame/shitgame01/activities/FirstPlayDisplayActivity.java
@@ -0,0 +1,68 @@
+package shitGame.shitgame01.activities;
+
+import java.sql.Date;
+
+import shitGame.shitgame01.R;
+
+import android.R.bool;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.provider.ContactsContract.CommonDataKinds.Event;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+public class FirstPlayDisplayActivity extends Activity {
+
+
+	long starttime=System.currentTimeMillis();//记录动画创建时间
+	/*
+	 * 最好不用currnetTimeMillis()，如果用户修改系统时间会影响结果
+	 * 
+	 */
+ 
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        
+        setContentView(R.layout.activity_start_firstplay);
+        this.init_pic();
+
+
+            
+    }
+    
+    
+    private void init_pic(){
+        ImageView loadingImageView=(ImageView)findViewById(R.id.imageView_loading);
+        loadingImageView.setImageResource(R.drawable.anim_start_loading);
+        
+       /*监听点击，播放完成后相应点击返回SelectMissionActivity*/
+        loadingImageView.setOnClickListener(new OnClickListener() {
+        	
+        	ImageView loadingImageView=(ImageView)findViewById(R.drawable.anim_start_loading);
+			@Override
+			public void onClick(View v) {
+				        /*需更新可维护性*/
+				        /*animationDrawable致崩溃*/
+						if(System.currentTimeMillis()-starttime>6000){
+				        Intent intent=new Intent(FirstPlayDisplayActivity.this,SelectMissionActivity.class);
+						FirstPlayDisplayActivity.this.startActivity(intent);			
+						FirstPlayDisplayActivity.this.finish();
+						
+						
+						}
+			}
+		});
+
+        AnimationDrawable animationDrawable;
+        animationDrawable=(AnimationDrawable)loadingImageView.getDrawable();
+        animationDrawable.start();
+    }
+    
+ 
+}
--- shitGame01/src/shitGame/shitgame01/activities/StartActivity.java
+++ shitGame01/src/shitGame/shitgame01/activities/StartActivity.java
@@ -1,13 +1,91 @@
 package shitGame.shitgame01.activities;
 
+import shitGame.shitgame01.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore.Audio.Media;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class StartActivity extends Activity{
+public class StartActivity extends Activity {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_start_menu);
+//music initation
+		Media media = new Media();
+//设置btn_start
+		Button bt_start=(Button)findViewById(R.id.btn_start);
+		bt_start.setOnClickListener(new OnClickListener(){
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				/*
+				 * 缺读记录判断是否第一次进入游戏
+				 * */
+				Intent intent=new Intent(StartActivity.this,FirstPlayDisplayActivity.class);
+				startActivity(intent);
+				
+			}
+		});
+		
+//设置btn_shop
+		Button bt_shop=(Button)findViewById(R.id.btn_shop);
+		bt_shop.setOnClickListener(new OnClickListener(){
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent=new Intent(StartActivity.this,ShopActivity.class);
+				startActivity(intent);				
+			}
+		});
+//设置btn_exit
+		Button bt_exit=(Button)findViewById(R.id.btn_exit);
+		bt_exit.setOnClickListener(new OnClickListener(){
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				finish();				
+			}
+		});
+//设置btn_music
+				Button bt_music=(Button)findViewById(R.id.btn_music);
+				bt_music.setOnClickListener(new OnClickListener(){
+					
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+
+						finish();	
+					}
+				});
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.startactivity_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
