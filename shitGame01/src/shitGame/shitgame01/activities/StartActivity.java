55420bde96fa36cae7c67c28451ab00484795161
jasonCarNormal0101 <jasoncar@outlook.com>
2014-11-23 11:55:45 +0800

    修改menu

--- shitGame01/src/shitGame/shitgame01/activities/StartActivity.java
+++ shitGame01/src/shitGame/shitgame01/activities/StartActivity.java
@@ -73,19 +73,8 @@ public class StartActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.startactivity_menu, menu);
-		return true;
+		return false;
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+
 }
