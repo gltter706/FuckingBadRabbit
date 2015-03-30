4d6c0f67b8fad4e4077346ec2782cf81395b77c0
jasonCarNormal0101 <jasoncar@outlook.com>
2015-03-30 23:31:27 +0800

    无用代码去除

--- shitGame01/src/shitGame/shitgame01/activities/FirstPlayDisplayActivity.java
+++ shitGame01/src/shitGame/shitgame01/activities/FirstPlayDisplayActivity.java
@@ -81,11 +81,7 @@ public class FirstPlayDisplayActivity extends Activity {
 								FirstPlayDisplayActivity.this.finish();
 							}
 
-						} else if (e2.getX() - e1.getX() >0 && counter > 0) {
-//							Log.d(TAG, "flingleft");
-//								counter--;
 						}
-						
 						return false;
 					}
 				});
@@ -97,34 +93,6 @@ public class FirstPlayDisplayActivity extends Activity {
 		return gestureDetector.onTouchEvent(event);
 
 	}
-	// private void init_pic(){
-	// ImageView
-	// loadingImageView=(ImageView)findViewById(R.id.imageView_loading);
-	// loadingImageView.setImageResource(R.anim.anim_start_loading);
-	//
-	// /*监听点击，播放完成后相应点击返回SelectMissionActivity*/
-	// loadingImageView.setOnClickListener(new OnClickListener() {
-	//
-	// ImageView
-	// loadingImageView=(ImageView)findViewById(R.anim.anim_start_loading);
-	// @Override
-	// public void onClick(View v) {
-	// /*需更新可维护性*/
-	// /*animationDrawable致崩溃*/
-	// if(System.currentTimeMillis()-starttime>6000){
-	// Intent intent=new
-	// Intent(FirstPlayDisplayActivity.this,SelectMissionActivity.class);
-	// FirstPlayDisplayActivity.this.startActivity(intent);
-	// FirstPlayDisplayActivity.this.finish();
-	//
-	//
-	// }
-	// }
-	// });
-	//
-	// AnimationDrawable animationDrawable;
-	// animationDrawable=(AnimationDrawable)loadingImageView.getDrawable();
-	// animationDrawable.start();
-	// }
+
 
 }
